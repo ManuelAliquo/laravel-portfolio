11f86c2d0916dd965d3d95d9dcb4a23144cea1b2
ManuelAliquo <manuelaliquo2103@gmail.com>
2026-06-30 17:17:53 +0200

    Add Project model and migration

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,8 @@
 @extends('layouts.app')
-
 @section('content')
+
 <div class="container">
     <h2 class="fs-4 text-secondary my-4">Dashboard</h2>
 </div>
+
 @endsection
--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -24,4 +24,5 @@
     </div>
     @endguest
 </div>
+
 @endsection
